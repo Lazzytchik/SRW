01575fb8ddcf076f6e236d18b70650b2b6ee91b3
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2021-04-14 20:36:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<html>
+<html lang="ru">
     <head>
         <title>Messanger</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="mystyle.css" rel="stylesheet" type="text/css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     </head>
     <body>
-        Test
-        <h1>Test</h1>
+        <div class="container">
+            <h1>Welcome to the test messanger</h1>
+            <h3>Log in</h3>
+            <form class="container">
+                <div class="container">
+                    <label>Login</label><br>
+                    <input type="text">
+                </div>
+                <div class="container">
+                    <label>Password</label><br>
+                    <input type="password">
+                </div>
+            </form>
+        </div>
     </body>
 </html>
