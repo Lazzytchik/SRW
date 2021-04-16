7ec41936092c000252383264c95dd1fb47c90cd7
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2021-04-16 17:59:37 +0300

    Registration and auth

--- index.php
+++ index.php
@@ -1,24 +1,72 @@
-<html lang="ru">
+<html lang="en">
     <head>
-        <title>Messanger</title>
+        <title>Log in</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="mystyle.css" rel="stylesheet" type="text/css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link href="css/mystyle.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="container">
+        <header>
             <h1>Welcome to the test messanger</h1>
             <h3>Log in</h3>
-            <form class="container">
-                <div class="container">
-                    <label>Login</label><br>
-                    <input type="text">
+        </header>
+        <div class="content">
+            <form class="form" method="post" action="index.php">
+                <div class="space">
+                    <label>Login</label>
+                    <input name="username" type="text">
                 </div>
-                <div class="container">
-                    <label>Password</label><br>
-                    <input type="password">
+                <div class="space">
+                    <label>Password</label>
+                    <input name="password" type="password">
                 </div>
+                <a href="register.php" class="space">
+                    <label class="reg">Register</label>
+                </a>
+                <input type="submit" name="log" class="space" value="Log in">
+                
+                <?php 
+                    
+                    $host = "localhost";
+				    $login = "root";
+				    $pas = "";
+                    
+                    $link = mysqli_connect($host, $login, $pas) or die("NO");
+				    mysqli_select_db($link, 'messanger') or die('NO DATABASE CALLED');
+                    
+                    $query = "SELECT * FROM users";
+                    $result = mysqli_query($link, $query);
+                    
+                
+                    if(isset($_POST['log'])){
+                        $username = $_POST['username'];
+                        $password = $_POST['password'];
+                                             
+                        if($username == ""){
+                            echo "Enter username";
+                        }
+                        else{
+                            $query = "SELECT username, password
+                                    FROM users
+                                    where username = '".$username."'";
+                            $user = mysqli_query($link, $query);
+                            
+                            if(mysqli_num_rows($user) == 0){
+                                echo "Wrong login";
+                            }
+                            else{
+                                $row = mysqli_fetch_assoc($user);
+                                if($row['password'] == $password){
+                                    echo "<script>window.location.href = 'messanger.php';</script>";
+                                }else{
+                                    echo "Wrong password";
+                                }
+                            }
+                            
+                        }
+                    }
+                ?>
+                
             </form>
         </div>
     </body>
---
+++ register.php
@@ -0,0 +1,69 @@
+<html lang="en">
+    <head>
+        <title>Register</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/mystyle.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header>
+            <h1>Welcome to the test messanger</h1>
+            <h3>Register</h3>
+        </header>
+        <div class="content">
+            <form class="form" method="post" action="register.php">
+                <div class="space">
+                    <label>Login</label>
+                    <input name="username" type="text">
+                </div>
+                <div class="space">
+                    <label>Password</label>
+                    <input name="password" type="password">
+                </div>
+                <div class="space">
+                    <label>Email</label>
+                    <input name="email" type="email">
+                </div>
+                <a href="index.php" class="space">
+                    <label class="reg">Log in</label>
+                </a>
+                <input type="submit" class="space" name="reg" value="Register">
+                <?php 
+                    
+                    $host = "localhost";
+				    $login = "root";
+				    $pas = "";
+                    
+                    $link = mysqli_connect($host, $login, $pas) or die("NO");
+				    mysqli_select_db($link, 'messanger') or die('NO DATABASE CALLED');
+                    
+                    $query = "SELECT * FROM users";
+                    $result = mysqli_query($link, $query);
+                    
+                    $id = mysqli_num_rows($result) + 1;
+                
+                    if(isset($_POST['reg'])){
+                        $username = $_POST['username'];
+                        $password = $_POST['password'];
+                        $email = $_POST['email'];
+                        
+                        //Запрос на проверку, сущетвует ли пользователь с таким же логином
+                        $uquery = "SELECT * FROM users WHERE username = '".$username."'";
+                        $uresult = mysqli_query($link, $uquery);
+                        
+                        if($username == "" or $password == "" or $email == ""){
+                            echo "Write normal data wtf";
+                        }else if(mysqli_num_rows($uresult) > 0){
+                            echo "Username has already taken";
+                        }else{
+                            $query = "INSERT INTO `users`(`user_id`, `username`, `password`, `email`) VALUES (".$id.",'".$username."','".$password."','".$email."')";
+                            mysqli_query($link, $query);
+                            echo "Registration is succsessfull";
+                        }
+                    }
+                ?>
+                
+            </form>
+        </div>
+    </body>
+</html>
